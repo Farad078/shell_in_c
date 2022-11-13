3f855fe2db7e5a595a1c3b875f599ecde948cd67
Farad078 <faradokorie@gmail.com>
2022-11-13 23:24:52 +0100

    new update

--- main.c
+++ main.c
@@ -1,49 +1,54 @@
 #include "main.h"
-
 /**
  * main - main program
  * Return: always return 0
  */
-
 int main(void)
 {
 	size_t n = 10, k;
 	char *new = malloc(sizeof(char) * n);
 	char **arg, *args, d[] = " ";
 	int i, counter = 1;
+	pid_t f;
 
 	if (new == NULL)
 		return (1);
-	printf("#cisfun$ ");
-	getline(&new, &n, stdin);
-	k = strlen(new);
-	new[k - 1] = '\0';
-	/* cmd = _strcat("/bin/", new); */
-	for (i = 0; new[i] != '\0'; i++)
+	while (true)
 	{
-		if (new[i] == ' ')
-			counter++;
+		printf("#cisfun$ ");
+		getline(&new, &n, stdin);
+		k = strlen(new);
+		new[k - 1] = '\0';
+		for (i = 0; new[i] != '\0'; i++)
+			if (new[i] == ' ')
+				counter++;
+		arg = malloc(sizeof(char *) * counter);
+		if (arg == NULL)
+			return (2);
+		for (i = 0; i <= counter; i++)
+		{
+			arg[i] = malloc(sizeof(char) * k);
+			if (arg[i] == NULL)
+				exit(1);
+		}
+		i = 0;
+		args = strtok(new, d);
+		while (args != NULL)
+		{
+			arg[i] = args;
+			/* printf("%d: %s %p\n", i, args, args);*/
+			args = strtok(NULL, d);
+			i++;
+		}
+		arg[i] = NULL;
+		f = fork();
+		if (f == 0)
+		{
+			if (execve(arg[0], arg, NULL) == -1)
+				perror("Error: ");
+		}
+		else
+			wait(NULL);
 	}
-	arg = malloc(sizeof(char *) * counter);
-	if (arg == NULL)
-		return (2);
-	for (i = 0; i <= counter; i++)
-	{
-		arg[i] = malloc(sizeof(char) * k);
-		if (arg[i] == NULL)
-			exit(1);
-	}
-	i = 0;
-	args = strtok(new, d);
-	while (args != NULL)
-	{
-		arg[i] = args;
-		/* printf("%d: %s %p\n", i, args, args);*/
-		args = strtok(NULL, d);
-		i++;
-	}
-	arg[i] = NULL;
-	if (execve(arg[0], arg, NULL) == -1)
-		perror("Error: ");
 	return (0);
 }
--- main.h
+++ main.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 char *_strcat(char *a, char *b);
 
